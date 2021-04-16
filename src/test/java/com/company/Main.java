08e0a673f7b7a2f737bdb6b5e282fd91f6be910d
JeffRisberg <j.s.risberg@gmail.com>
2021-04-15 23:28:32 -0700

    set up echo container

--- src/test/java/com/company/Main.java
+++ src/test/java/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
-import java.sql.SQLException;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.testcontainers.containers.GenericContainer;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.shaded.okhttp3.*;
+import org.testcontainers.utility.DockerImageName;
+import org.testng.annotations.*;
 
 @Slf4j
 public class Main {
@@ -13,12 +15,36 @@ public class Main {
 
   @BeforeClass
   public static void setup() {
-    container = new GenericContainer<>("hashicorp/http-echo");
+    container =
+        new GenericContainer<>(DockerImageName.parse("testcontainers/helloworld:1.1.0"))
+            .withExposedPorts(8080, 8081)
+            .withLogConsumer(new Slf4jLogConsumer(log));
+
+    container.start();
     log.info("created container");
   }
 
+  @AfterClass
+  public static void shutdown() {
+    container.stop();
+    log.info("stopped container");
+  }
+
   @Test
-  public void sampleTestMethod1() throws SQLException {
+  public void sampleTestMethod1() throws IOException {
+    String host = container.getContainerIpAddress();
+    String port = container.getFirstMappedPort().toString();
+
+    String url = "http://" + host + ":" + port;
+
     // rest calls to container
+    OkHttpClient client = new OkHttpClient();
+
+    Request request = new Request.Builder().url(url).build();
+
+    try (Response response = client.newCall(request).execute()) {
+      String x = response.body().string();
+      System.out.println(x);
+    }
   }
 }
