3fafb8b934f254b27621821e5fa25912f63df70c
JeffRisberg <j.s.risberg@gmail.com>
2021-04-16 00:22:14 -0700

    set up echo container

--- src/test/java/com/company/Main.java
+++ src/test/java/com/company/Main.java
@@ -1,22 +1,29 @@
 package com.company;
 
+import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.shaded.okhttp3.*;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import org.testcontainers.utility.DockerImageName;
 import org.testng.annotations.*;
 
 @Slf4j
 public class Main {
 
+  public static final MediaType JSON = okhttp3.MediaType.parse("application/json; charset=utf-8");
+
   static GenericContainer<?> container;
 
   @BeforeClass
   public static void setup() {
     container =
-        new GenericContainer<>(DockerImageName.parse("testcontainers/helloworld:1.1.0"))
+        new GenericContainer<>(DockerImageName.parse("ealen/echo-server"))
             .withExposedPorts(8080, 8081)
             .withLogConsumer(new Slf4jLogConsumer(log));
 
@@ -32,6 +39,27 @@ public class Main {
 
   @Test
   public void sampleTestMethod1() throws IOException {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonParser jsonParser = new JsonParser();
+
+    JsonObject accessParamMap = new JsonObject();
+    accessParamMap.addProperty("buttonElementId", "submit_button");
+    accessParamMap.addProperty("colorTheme", "green");
+    accessParamMap.addProperty("deployUrl", "http://ncoding.io");
+    accessParamMap.addProperty("descriptionElementId", "submit_description");
+    accessParamMap.addProperty("formElementId", "submit_form");
+    accessParamMap.addProperty("subjectElementId", "submit_subject");
+    accessParamMap.addProperty("restServer", "https://api.prod2.company.com/restserver");
+    accessParamMap.addProperty("successUrl", "http://ncoding.io/index.html");
+
+    JsonObject bodyJson = new JsonObject();
+    bodyJson.add("accessParamMap", accessParamMap);
+    bodyJson.addProperty("appKey", "12345678");
+    bodyJson.addProperty("token", "qwertyuiop");
+
+    RequestBody body = RequestBody.create(JSON, bodyJson.toString());
+
     String host = container.getContainerIpAddress();
     String port = container.getFirstMappedPort().toString();
 
@@ -40,7 +68,8 @@ public class Main {
     // rest calls to container
     OkHttpClient client = new OkHttpClient();
 
-    Request request = new Request.Builder().url(url).build();
+    Request request =
+        new Request.Builder().url(url).header(CONTENT_TYPE, JSON.toString()).post(body).build();
 
     try (Response response = client.newCall(request).execute()) {
       String x = response.body().string();
